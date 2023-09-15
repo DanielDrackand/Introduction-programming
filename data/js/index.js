e9dc5ada3e4ec338f2e3704a22bea8fb96eacc30
Lapp Lucas Daniel <daniellapp2010@gmail.com>
2023-09-15 19:46:02 -0300

    Por fin , la version de celular esta lista

---
+++ data/js/index.js
@@ -0,0 +1,14 @@
+//Console
+console.log("hi");
+//llamadas
+const icon = document.querySelector('.icon');
+
+//funciones 
+
+//style
+//hacer aparecier el icono {}
+setTimeout(() => {
+    icon.style.opacity = 1;
+},900)
+
+//code
